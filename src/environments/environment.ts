609c6e879bf62be0b3ff40030fa3db5576e50b93
Rodrigo Hillion <rohillion@Rodrigos-Mini.fritz.box>
2019-04-27 12:17:07 +0100

    menu with button to accounts and environments fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,14 @@
 
 export const environment = {
   production: false,
-  apiKey: 'AIzaSyA7KxEdEXa3wqBvnTUqdy7CGg44xONIafw',
-  authDomain: 'fireforce-io.firebaseapp.com',
-  databaseURL: 'https://fireforce-io.firebaseio.com',
-  projectId: 'fireforce-io',
-  storageBucket: 'fireforce-io.appspot.com',
-  messagingSenderId: '539804404434'
+  firebase: {
+    apiKey: 'AIzaSyA7KxEdEXa3wqBvnTUqdy7CGg44xONIafw',
+    authDomain: 'fireforce-io.firebaseapp.com',
+    databaseURL: 'https://fireforce-io.firebaseio.com',
+    projectId: 'fireforce-io',
+    storageBucket: 'fireforce-io.appspot.com',
+    messagingSenderId: '539804404434'
+  }
 };
 
 /*
