4ebbd115e23ede8f9cb4acf7ee7b06039fbfe7f8
Rodrigo Hillion <rohillion@Rodrigos-Mini.fritz.box>
2019-04-28 15:12:26 +0100

    firebase project setup for hosting, database and functions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA7KxEdEXa3wqBvnTUqdy7CGg44xONIafw',
+    authDomain: 'fireforce-io.firebaseapp.com',
+    databaseURL: 'https://fireforce-io.firebaseio.com',
+    projectId: 'fireforce-io',
+    storageBucket: 'fireforce-io.appspot.com',
+    messagingSenderId: '539804404434'
+  }
 };
